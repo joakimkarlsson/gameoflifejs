d7fb81c69eb99c34aabd8afcddf6c0d3419b9a38
Joakim Karlsson <joakim@jkarlsson.com>
2012-06-06 10:20:43 +0200

    Basic drawing implemented

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 require(["utils/gameboard"], function(gameboard) {
-  gameboard.clear();
+  
+  var position = {x: 0, y: 0};
+  setInterval(function() {
+    gameboard.clear();
+    position.x += 1;
+    position.y += 1;
+    gameboard.drawCellAt(position);
+  }, 100);
 });
--- scripts/utils/gameboard.js
+++ scripts/utils/gameboard.js
@@ -3,10 +3,37 @@ define(function() {
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext("2d");
 
+  var dimensions = {height: 100, width: 100};
+
+  var transformGameCoordsToCanvasCoords = function(gameCoords) {
+      var cellWidth = canvas.width / dimensions.width;
+      var cellHeight = canvas.height / dimensions.height;
+      var left = gameCoords.x * cellWidth;
+      var top = gameCoords.y * cellHeight;
+
+      return {left: left, top: top, width: cellWidth, height: cellHeight};
+  };
+
   return {
     clear: function() {
-      context.fillStyle = "rgb(200, 34, 55)";
-      context.fillRect(10, 10, 200, 200);
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    },
+    
+    setDimensions: function(params) {
+      dimensions.width = params.width;
+      dimensions.height = params.height;
+    },
+
+    drawCellAt: function(coords) {
+      var c = transformGameCoordsToCanvasCoords(coords);
+
+      context.fillStyle = "rgb(200, 34, 23)";
+      context.fillRect(c.left, c.top, c.width, c.height);
+    },
+
+    clearCellAt: function(coords) {
+      var c = transformGameCoordsToCanvasCoords(coords);
+      context.clearRect(c.left, c.top, c.width, c.height);
     }
   };
 });
